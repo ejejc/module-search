c7d79f98bf7828dacb77618daa5083d5e8114e7e
eunjis <ejejc@naver.com>
2023-03-20 23:11:19 +0900

    프로젝트 생성 및 멀티 모듈 구성.

---
+++ module-rds/src/test/java/com/example/modulerds/domain/WebTest.java
@@ -0,0 +1,12 @@
+package com.example.modulerds.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+public class WebTest {
+
+    @Test
+    void test1() {}
+
+}
